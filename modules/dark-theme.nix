9b9bb02ded860b57b22eb3a5f3d8199d5e9ad8a4
dooory <78720364+dooory@users.noreply.github.com>
2025-08-06 18:19:37 +1200

    feat(modules): add dark-theme.nix core module

---
+++ modules/dark-theme.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, ... }:
+
+{
+  # Enable and configure dconf (for GTK preference applications)
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      gtk-theme = "Adwaita-dark";
+    };
+  };
+
+  # Enable and configure GTK themes
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Adwaita-dark";
+      package = pkgs.gnome-themes-extra;
+    };
+    gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
+    gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
+  };
+
+  # Configure Qt applications
+  qt = {
+    enable = true;
+    platformTheme.name = "Adwaita-dark";
+    style = {
+      name = "Adwaita-dark";
+      package = pkgs.adwaita-qt;
+    };
+  };
+}
+
