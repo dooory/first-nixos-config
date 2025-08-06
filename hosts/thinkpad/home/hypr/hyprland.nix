29f8dfa905e8cacd377178b326a714f39a10264d
dooory <78720364+dooory@users.noreply.github.com>
2025-08-06 18:15:24 +1200

    chore(hypr): add playerctl + pamixer for multimedia controls

--- hosts/thinkpad/home/hypr/hyprland.nix
+++ hosts/thinkpad/home/hypr/hyprland.nix
@@ -1,12 +1,20 @@
+{ pkgs, ... }:
+
 {
 	imports = [
 		./keybinds.nix
 		./variables.nix
 	];
+
 	wayland.windowManager.hyprland = {
 		enable = true;
 		extraConfig = ''
 			${builtins.readFile ./hyprland.conf}
 		'';
 	};
+
+	home.packages = with pkgs; [
+		playerctl
+		pamixer
+	];
 }
