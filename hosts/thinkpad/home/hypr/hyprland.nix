79bb556b6f1b9202deac72a38b6f44586a2b152b
dooory <78720364+dooory@users.noreply.github.com>
2025-08-06 18:47:57 +1200

    chore(hypr): remove hyprland.conf and any references in hyprland.nix
    I've finished importing all of the configs from my old config and the default config

--- hosts/thinkpad/home/hypr/hyprland.nix
+++ hosts/thinkpad/home/hypr/hyprland.nix
@@ -10,9 +10,6 @@
 
 	wayland.windowManager.hyprland = {
 		enable = true;
-		extraConfig = ''
-			${builtins.readFile ./hyprland.conf}
-		'';
 		settings = {
 			windowrule = [
 				"suppressevent maximize, class:.*"
