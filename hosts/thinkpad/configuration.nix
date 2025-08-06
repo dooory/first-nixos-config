72b3cb23e2fd94db1ffda5e34a3d957253b06df8
dooory <78720364+dooory@users.noreply.github.com>
2025-08-06 16:24:13 +1200

    chore: cleanup configuration.nix

--- hosts/thinkpad/configuration.nix
+++ hosts/thinkpad/configuration.nix
@@ -6,28 +6,18 @@
 
 {
 	imports =
-		[ # Include the results of the hardware scan.
+		[
 			./hardware-configuration.nix
 		];
 
-	# Bootloader.
 	boot.loader.systemd-boot.enable = true;
 	boot.loader.efi.canTouchEfiVariables = true;
 
-	networking.hostName = "thinkpad"; # Define your hostname.
-	# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-	# Configure network proxy if necessary
-	# networking.proxy.default = "http://user:password@proxy:port/";
-	# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-	# Enable networking
+	networking.hostName = "thinkpad";
 	networking.networkmanager.enable = true;
 
-	# Set your time zone.
 	time.timeZone = "Pacific/Auckland";
 
-	# Select internationalisation properties.
 	i18n.defaultLocale = "en_NZ.UTF-8";
 
 	i18n.extraLocaleSettings = {
@@ -42,20 +32,16 @@
 		LC_TIME = "en_NZ.UTF-8";
 	};
 
-	# Enable the GNOME Desktop Environment.
 	services.displayManager.gdm.enable = true;
 	services.desktopManager.gnome.enable = true;
 
-	# Configure keymap in X11
 	services.xserver.xkb = {
 		layout = "nz";
 		variant = "";
 	};
 
-	# Enable CUPS to print documents.
 	services.printing.enable = true;
 
-	# Enable sound with pipewire.
 	services.pulseaudio.enable = false;
 	security.rtkit.enable = true;
 	services.pipewire = {
@@ -63,46 +49,28 @@
 		alsa.enable = true;
 		alsa.support32Bit = true;
 		pulse.enable = true;
-		# If you want to use JACK applications, uncomment this
-		#jack.enable = true;
-
-		# use the example session manager (no others are packaged yet so this is enabled by default,
-		# no need to redefine it in your config for now)
-		#media-session.enable = true;
 	};
 
-	# Enable touchpad support (enabled default in most desktopManager).
-	# services.xserver.libinput.enable = true;
-
-	# Define a user account. Don't forget to set a password with ‘passwd’.
 	users.users.dory = {
 		isNormalUser = true;
 		description = "dory";
 		extraGroups = [ "networkmanager" "wheel" ];
 		packages = with pkgs; [
-			#  thunderbird
 		];
 	};
 
-	# Enable automatic login for the user.
 	services.displayManager.autoLogin.enable = true;
 	services.displayManager.autoLogin.user = "dory";
 
-	# Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
 	systemd.services."getty@tty1".enable = false;
 	systemd.services."autovt@tty1".enable = false;
 
-	# Install firefox.
 	programs.firefox.enable = true;
 
-	# Allow unfree packages
 	nixpkgs.config.allowUnfree = true;
 
-	# List packages installed in system profile. To search, run:
-	# $ nix search wget
 	environment.systemPackages = with pkgs; [
-		#  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-		#  wget
+
 	];
 
 	services.keyd = {
@@ -120,7 +88,7 @@
 		};
 	};
 
-	system.stateVersion = "25.05"; # Did you read the comment?
+	system.stateVersion = "25.05";
 
 	nix = {
 		settings.experimental-features = [ "nix-command" "flakes" ];
