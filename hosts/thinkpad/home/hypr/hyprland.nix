006300277ed1130cd490798bf27c5c65d861a74f
dooory <78720364+dooory@users.noreply.github.com>
2025-08-06 18:44:29 +1200

    chore(hypr): import window rules from default config

--- hosts/thinkpad/home/hypr/hyprland.nix
+++ hosts/thinkpad/home/hypr/hyprland.nix
@@ -13,6 +13,12 @@
 		extraConfig = ''
 			${builtins.readFile ./hyprland.conf}
 		'';
+		settings = {
+			windowrule = [
+				"suppressevent maximize, class:.*"
+				"nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
+			];
+		};
 	};
 
 	home.packages = with pkgs; [
