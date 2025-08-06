c87b424aa2f0846cba5d8725e0b911c29904f6ed
dooory <78720364+dooory@users.noreply.github.com>
2025-08-06 18:25:13 +1200

    chore(hypr): create appearance.nix

---
+++ hosts/thinkpad/home/hypr/appearance.nix
@@ -0,0 +1,14 @@
+{
+	wayland.windowManager.hyprland = {
+		settings = {
+			animations.enabled = false;
+
+			decoration = {
+				shadow.enabled = false;
+				blur.enabled = false;
+			};
+
+			misc.vfr = true;
+		};
+	};
+}
