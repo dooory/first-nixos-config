65df2a738cf1e69dc5f5c09ccd1e5bb0d7f25256
dooory <78720364+dooory@users.noreply.github.com>
2025-08-06 16:28:18 +1200

    feat: change bootloader from systemd to grub

--- hosts/thinkpad/configuration.nix
+++ hosts/thinkpad/configuration.nix
@@ -10,8 +10,13 @@
 			./hardware-configuration.nix
 		];
 
-	boot.loader.systemd-boot.enable = true;
 	boot.loader.efi.canTouchEfiVariables = true;
+
+	boot.loader.grub = {
+		enable = true;
+		device = "nodev";
+		efiSupport = true;
+	};
 
 	networking.hostName = "thinkpad";
 	networking.networkmanager.enable = true;
