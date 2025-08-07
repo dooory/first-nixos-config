b43fa6f8057d278f88d2207d393cb588352d48c7
dooory <78720364+dooory@users.noreply.github.com>
2025-08-07 16:47:47 +1200

    style(hypr): add color variables to use with hyprland

--- hosts/thinkpad/home/hypr/variables.nix
+++ hosts/thinkpad/home/hypr/variables.nix
@@ -5,6 +5,14 @@
 			"$terminal" = "kitty";
 			"$launcher" = "wofi --show drun";
 			"$browser" = "floorp";
+
+			# decimal color codes aren't allowed to have spaces between values
+			"$primary-color" = "rgb(176,97,133)";
+			"$secondary-color" = "rgb(235,220,179)";
+			"$teritary-color" = "rgb(83,79,72)";
+			"$background-color" = "rgb(30,33,33)";
+
+			"$inactive-border" = "rgba(235,220,179,0.35)";
 		};
 	};
 }
