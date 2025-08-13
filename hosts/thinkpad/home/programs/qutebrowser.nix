0938f5c5e6506fe059e1bce64906ee4db230f29e
dooory <78720364+dooory@users.noreply.github.com>
2025-08-13 16:19:49 +1200

    feat(qutebrowser): set default searchEngine + search aliases

--- hosts/thinkpad/home/programs/qutebrowser.nix
+++ hosts/thinkpad/home/programs/qutebrowser.nix
@@ -2,6 +2,14 @@
 	programs.qutebrowser = {
 		enable = true;
 
+		searchEngines = {
+			DEFAULT = "https://www.google.com/search?q={}";
+			gh = "https://www.github.com/search?q={}";
+			yt = "https://www.youtube.com/results?search_query={}";
+			mn = "https://www.mynixos.com/search?q={}";
+			nw = "https://nixos.wiki/index.php?search={}";
+		};
+
 		keyBindings = {
 			normal = {
 				"<shift-l>" = "tab-next";
