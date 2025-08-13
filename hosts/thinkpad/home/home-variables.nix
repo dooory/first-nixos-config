f3cb361e3ce9bc353af481558e7668511493dc3a
dooory <78720364+dooory@users.noreply.github.com>
2025-08-13 17:03:53 +1200

    feat(home): add hexadecimal values to each theme variables color attribute setw

--- hosts/thinkpad/home/home-variables.nix
+++ hosts/thinkpad/home/home-variables.nix
@@ -11,24 +11,28 @@
 			g = 97;
 			b = 133;
 			rgb = "${toString r},${toString g},${toString b}";
+			hex = "#B06185";
 		};
 		secondary = rec {
 			r = 235;
 			g = 220;
 			b = 179;
 			rgb = "${toString r},${toString g},${toString b}";
+			hex = "#EBDCB3";
 		};
 		teritary = rec {
 			r = 83;
 			g = 79;
 			b = 72;
 			rgb = "${toString r},${toString g},${toString b}";
+			hex = "#534F48";
 		};
 		background = rec {
 			r = 30;
 			g = 33;
 			b = 33;
 			rgb = "${toString r},${toString g},${toString b}";
+			hex = "#1E2121";
 		};
 	};
 }
