9d494e2cce378d57ca2528f313551327f164bb61
dooory <78720364+dooory@users.noreply.github.com>
2025-08-19 13:32:03 +1200

    style(wofi): add background-color and border to selected entry + set border-radius

--- hosts/thinkpad/home/programs/wofi.nix
+++ hosts/thinkpad/home/programs/wofi.nix
@@ -7,12 +7,18 @@ in
 		style = ''
 			* {
 				font-family: "MesloLGM Nerd Font Propo";
+				border-radius: 0;
 			}
 
 			window, entry {
 				background-color: rgba(${theme.background.rgb}, 0.9);
 				color: ${theme.secondary.hex};
 				border: 2px solid ${theme.secondary.hex};
+			}
+
+			#entry:selected {
+				background-color: ${theme.primary.hex};
+				border: 2px solid ${theme.primary.hex};
 			}
 		'';
 	};
